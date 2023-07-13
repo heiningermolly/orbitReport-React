4b522a43bdcd865dd2b2ac56299fa9b517800151
Molly Heininger <heininger.molly@gmail.com>
2023-07-13 09:35:49 -0500

    completed first variable per instruction 3.3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/Banner";
+import { useState } from "react";
+import satData from "./components/satData";
 
 
 function App() {
+  const [sat, setSat] = useState(satData);
   return (
     <div>
+      <Banner />
       <Buttons />
       <Table />
     </div>
